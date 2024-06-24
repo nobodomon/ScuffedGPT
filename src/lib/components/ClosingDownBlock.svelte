4611b2cdb28f604babc2d96942dbea5682d45cc5
nobodomon <nobodomon@gmail.com>
2024-06-24 23:29:47 +0800

    closing down with links to self deploy

--- src/lib/components/ClosingDownBlock.svelte
+++ src/lib/components/ClosingDownBlock.svelte
@@ -2,11 +2,15 @@
 
 </script>
 
-<div class='w-full h-full flex items-center justify-center bg-neutral text-neutral-content self-center'>
+<div class='w-full h-full flex flex-col gap-4 items-center justify-center bg-neutral text-neutral-content self-center'>
     <h1 class='text-2xl'>
         ScuffedGPT is shutting down
     </h1>
     <h2 class="text-xl">
         This site will close down by 1st July 2024
     </h2>
+    <p class="flex flex-col items-center">
+        If you want to deploy it yourself, 
+        <a class='link' href='https://github.com/nobodomon/ScuffedGPT'>https://github.com/nobodomon/ScuffedGPT</a>
+    </p>
 </div>
