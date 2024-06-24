7bd019fdaeaa296100c19e35c46979e1f9c5af9a
nobodomon <nobodomon@gmail.com>
2024-06-24 23:26:41 +0800

    shutting down

---
+++ src/lib/components/ClosingDownBlock.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<div class='w-full h-full flex items-center justify-center bg-neutral text-neutral-content self-center'>
+    <h1 class='text-2xl'>
+        ScuffedGPT is shutting down
+    </h1>
+</div>
--- src/routes/transcribe/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import Transcribe from '$lib/components/Transcribe.svelte'
-import type { PageData } from './$types';
-    
-    export let data: PageData;
-</script>
-
-<Transcribe></Transcribe>
